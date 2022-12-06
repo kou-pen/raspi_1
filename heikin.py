66cac03b2182375db282589fbd757e735abf0be8
kohki sakakibara <kou-pen@outlook.jp>
2022-12-06 10:07:21 +0900

    add heikin kuku

---
+++ heikin.py
@@ -0,0 +1,13 @@
+n = int(input('学生の人数は?>'))
+sump = 0
+student = [0] * n
+for i in range(n):
+    point = int(input('%d人目の点数は?>'%(i+1)))
+    student[i] = point
+    sump = sump + point
+avg = sump / n
+for i in range(n):
+    print('%d人目の点数は%d点'%(i,student[i]))
+print('平均点は%5.1f点です'%avg)
+
+    
--- kuku.py
+++ kuku.py
@@ -1,5 +1,5 @@
-for i in range(1,10):
-    for j in range(1,10):
+for i in range(1,21):
+    for j in range(1,21):
         solve = i * j
-        print('%3d'%solve,end='')
+        print('%4d'%solve,end='')
     print()
